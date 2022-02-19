04016366b591a25c87b418ac55b4a5218698fc0e
Febzey <braydent1235@gmail.com>
2022-02-19 02:00:42 -0600

    k

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -1,9 +1,9 @@
 import chalk from "chalk";
+import type { Guild } from 'discord.js';
 
 export default class Logger {
-    discordReady = (user: string) => console.info(chalk.bgBlack(chalk.green("DISCORD BOT CONNECTED. USER: " + user)))
+    discordReady      = (user: string) => console.info(chalk.bgBlack(chalk.green("DISCORD BOT CONNECTED. USER: " + user)))
     databaseConnected = (user: string, host: string) => console.info(chalk.green(`Database connection established.`,chalk.blue(`[USER]:`),chalk.cyan(`${user}`),chalk.blue(`[HOST]:`),chalk.cyan(`${host}`)))
-    GuildJoined = (guild) => console.info(chalk.green("Joined the guild:"), chalk.cyan(guild.name), chalk.blue(`Time: ${Date.now()}`))
-    newGuildJoined = (guild) => console.info(chalk.green("Joined the guild:"),chalk.cyan(guild.name),chalk.green("for the first time!"),chalk.blue(`Time: ${Date.now()}`))
-
+    GuildJoined       = (guild: Guild) => console.info(chalk.green("Joined the guild:"), chalk.cyan(guild.name), chalk.blue(`Time: ${Date.now()}`))
+    newGuildJoined    = (guild: Guild) => console.info(chalk.green("Joined the guild:"),chalk.cyan(guild.name),chalk.green("for the first time!"),chalk.blue(`Time: ${Date.now()}`))
 }
