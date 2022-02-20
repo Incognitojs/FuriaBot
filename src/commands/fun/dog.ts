d2516386f46a7f0b63632a4a51a656ea66166991
Incognito.js <84217223+Incognitojs@users.noreply.github.com>
2022-02-20 14:58:32 +0800

    okokokokokok - Febzey

---
+++ src/commands/fun/dog.ts
@@ -0,0 +1,26 @@
+import type { CommandInteraction, ColorResolvable } from "discord.js";
+import type { guild }                               from '../../../index';
+import { en_text, colors }                          from '../../struct/config.js';
+import fetch                                        from 'node-fetch'
+
+export default {
+    data: en_text.command.cat.data,
+    run: async (interaction: CommandInteraction, guild: guild) => {
+        const _data = await fetch('https://api.thedogapi.com/v1/images/search');
+        const data: any = await _data.json();
+
+        const randomColor = colors.colors[Math.floor(Math.random() * colors.colors.length)];
+        const hex = randomColor[Object.keys(randomColor)[0]] as ColorResolvable;
+   
+
+        interaction.reply({
+            embeds: [{
+                description: "",
+                color: hex,
+                image: {
+                    url: `${data[0].url}`
+                }
+            }]
+        })
+    }
+}
